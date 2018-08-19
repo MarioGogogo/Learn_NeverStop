7aa4c1726ce32ca7d2a0eef5c46886e30b8c152a
JasonWilltrue <148373644@qq.com>
2018-08-19 23:38:33 +0800

    面向对象 class的如何定义，继承的概念

--- 设计模式/src/index.js
+++ 设计模式/src/index.js
@@ -1,9 +1,31 @@
-class Person{
-    constructor(name){
-        this.name = name;
-    }
-}
+/**
+ * 类的继承
+ */
 
-let p = new Person('老师');
+ class Father{
+     constructor(name,age){
+         this.name = name;
+         this.age = age;
+     };
+     eat(){
+         alert(`${this.name}是个人名，还有她的年龄${this.age}`);
 
-alert(p.name);
+     }
+     speak(){
+       alert(`她说话了${this.name}这是她的名字`);
+     }
+
+ }
+
+ class Son extends Father{
+     constructor(name,age,number){
+         super(name,age);
+         this.number = number;
+     }
+     study(){
+         alert(`现在她的名字是${this.name},她考试得分${this.number}`);
+     }
+ }
+
+ let Dong = new Son('董小姐','18','80');
+ Dong.study();
