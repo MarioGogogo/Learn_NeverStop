de1d1c9996975b58baa637257b0c8e704374c3f9
Mario <148373644@qq.com>
2019-04-22 13:03:02 +0800

    add learn01

--- Vue源码分析/剖析vue实现原理，自己动手实现mvvm/js/mvvm.js
+++ Vue源码分析/剖析vue实现原理，自己动手实现mvvm/js/mvvm.js
@@ -17,7 +17,7 @@ function MVVM(options) {
     });
 
     this._initComputed();
-
+     
     observe(data, this);
     // 创建一个编译对象
     this.$compile = new Compile(options.el || document.body, this)
